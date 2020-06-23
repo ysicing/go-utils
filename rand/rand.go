435b6df67180cf18c85a092949e7097bc1c0fa8c
ysicing <i@ysicing.me>
2020-06-23 08:03:29 +0800

    improvement: update code

--- rand/rand.go
+++ rand/rand.go
@@ -3,12 +3,29 @@
 
 package rand
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
+// // Rand 随机数
 func Rand() int {
 	return rand.Int()
 }
 
+// RandByNum 随机数
 func RandByNum(num int) int {
 	return rand.Intn(num)
+}
+
+// RandString 生成随机字符串
+func RandString(len int) string {
+	var r *rand.Rand
+	r = rand.New(rand.NewSource(time.Now().Unix()))
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		b := r.Intn(26) + 65
+		bytes[i] = byte(b)
+	}
+	return string(bytes)
 }
