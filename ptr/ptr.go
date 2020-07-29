f625397c282fdd647b640b36e94a1e835c671eb8
ysicing <i@ysicing.me>
2020-07-29 23:07:07 +0800

    feat: add bool ptr

--- ptr/ptr.go
+++ ptr/ptr.go
@@ -4,10 +4,13 @@
 package ptr
 
 // Int32Ptr int32指针
-func Int32Ptr(i int32) *int32 { return &i }
+func Int32Ptr(p int32) *int32 { return &p }
 
 // Int64Ptr int64指针
 func Int64Ptr(p int64) *int64 { return &p }
 
 // StringPtr 字符串指针
 func StringPtr(p string) *string { return &p }
+
+// BoolPrt bool指针
+func BoolPtr(p bool) *bool { return &p}
